65a7e1ef77583faad4d1e3d7988aea3d7d558780
Pat Maddox <patmaddox@me.com>
2010-08-18 16:51:24 -0700

    rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DemoRails3App::Application.config.secret_token = '1ff5eebe45446f700ee12eb900be869b323495e6caf2814ff2a1cfd80a95516ce0f5a4edde3f91c988ba4270396ff9c5b497d586a5f4ee0bfcf3b6a176c64f9f'
---
+++ config/routes.rb
@@ -0,0 +1,3 @@
+DemoRails3App::Application.routes.draw do
+  root :to => "home#index"
+end
